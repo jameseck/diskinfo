a379dcca455958b3d4bd238b2dce62d76e5a642d
James Eckersall <james.eckersall@gmail.com>
2021-03-23 11:33:34 +0000

    rewrite

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module jehome.co.uk/diskinfo
 
 go 1.16
+
+require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/kataras/tablewriter v0.0.0-20180708051242-e063d29b7c23
+	github.com/lensesio/tableprinter v0.0.0-20201125135848-89e81fc956e7
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/tomlazar/table v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,151 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
+	//	"log"
 	"os"
+	"os/exec"
 	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/kataras/tablewriter"
+	"github.com/lensesio/tableprinter"
 )
 
+type Disk struct {
+	Disk       string `header:"disk"`
+	Md         string `header:"md"`
+	Lvm        string `header:"lvm"`
+	Zfs        string `header:"zfs"`
+	Luks       string `header:"luks"`
+	Size       string `header:"size"`
+	Ssd        string `header:"ssd"`
+	InUse      string `header:"in use"`
+	LocalMount string `header:"localmount"`
+	ID         string `header:"id"`
+	Slot       string `header:"slot"`
+}
+
+// rows << ['Disk', 'md', 'lvm', 'zfs', 'luks', 'Size', 'SSD?', 'In Use', 'Local Mount?', 'ID', 'Slot']
+
+type Blockdevice struct {
+	Name       string `json:"name" header:"disk"`
+	Fstype     string `json:"fstype"`
+	Label      string `json:"label"`
+	Uuid       string `json:"uuid"`
+	Mountpoint string `json:"mountpoint" header:"mountpoint"`
+	Md         string `header:"md"`
+	Lvm        string `header:"lvm"`
+	Zfs        string `header:"zfs"`
+	Luks       string `header:"luks"`
+	Size       string `header:"size"`
+	Ssd        string `header:"ssd"`
+	InUse      string `header:"in use"`
+	ID         string `header:"id"`
+	Slot       string `header:"slot"`
+}
+
+type Lsblk struct {
+	Blockdevices []Blockdevice `json:"blockdevices"`
+}
+
 func main() {
+
 	r, _ := regexp.Compile("^(sd[a-z]+|nvme[0-9]+n[0-9]+)$")
-	files, err := getDevices("/dev", r)
-	if err != nil {
-		log.Fatal(err)
+
+	blk := lsblk(r)
+
+	for f := range blk.Blockdevices {
+		blk.Blockdevices[f].Size, _ = getDiskSizeGB(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].Md := isMd(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].Lvm := isLvm(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].Zfs := isZfs(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].Luks := isLuks(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].Ssd := isSsd(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].inUse := inUse(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].id := id(blk.Blockdevices[f].Name)
+		//blk.Blockdevices[f].slot := slot(blk.Blockdevices[f].Name)
+
 	}
-	fmt.Printf("%v\n", files)
+
+	printer := tableprinter.New(os.Stdout)
+	sort.Slice(blk.Blockdevices, func(i, j int) bool {
+		return blk.Blockdevices[j].Name > blk.Blockdevices[i].Name
+	})
+	printer.BorderTop, printer.BorderBottom, printer.BorderLeft, printer.BorderRight = true, true, true, true
+	printer.CenterSeparator = "│"
+	printer.ColumnSeparator = "│"
+	printer.RowSeparator = "─"
+	printer.HeaderBgColor = tablewriter.BgBlackColor
+	printer.HeaderFgColor = tablewriter.FgGreenColor
+	printer.DefaultAlignment = tableprinter.AlignRight // Set Alignment
+
+	// Print the slice of structs as table, as shown above.
+	printer.Print(blk.Blockdevices)
+
+	//	fmt.Printf("\n\n\n")
+	//	fmt.Printf("blk type: %T\n", blk)
+	//	fmt.Printf("\n\n\n")
+	//
+	//	for bd := range blk.Blockdevices {
+	//		fmt.Printf("%s\n", blk.Blockdevices[bd].Name)
+	//	}
+	//
+}
+
+func lsblk(r *regexp.Regexp) (out Lsblk) {
+
+	lsblkOut, _ := exec.Command("lsblk", "--fs", "-J").Output()
+
+	var tmpOut Lsblk
+
+	json.Unmarshal([]byte(lsblkOut), &tmpOut)
+
+	for d := range tmpOut.Blockdevices {
+		if r.MatchString(tmpOut.Blockdevices[d].Name) {
+			out.Blockdevices = append(out.Blockdevices, tmpOut.Blockdevices[d])
+		}
+	}
+
+	fmt.Printf("out type: %T\n", tmpOut)
+	fmt.Printf("length tmpout: %d\n", len(out.Blockdevices))
+	return out
+}
+
+func isMd(device string) string {
+	return "N"
+}
+func isLvm(device string) string {
+	return "N"
+}
+func isZfs(device string) string {
+	return "N"
+}
+func isLuks(device string) string {
+	return "N"
+}
+func isSsd(device string) string {
+	return "N"
+}
+func inUse(device string) string {
+	return "N"
+}
+func localMount(device string) string {
+	return "N"
+}
+func id(device string) string {
+	return "N"
+}
+func slot(device string) string {
+	return "N"
 }
 
 func getDevices(dir string, r *regexp.Regexp) (names []string, err error) {
-	files, err := os.ReadDir("/dev")
+	files, err := os.ReadDir(dir)
 	if err != nil {
 		return names, err
 	}
@@ -28,4 +156,16 @@ func getDevices(dir string, r *regexp.Regexp) (names []string, err error) {
 		}
 	}
 	return names, err
+}
+
+func getDiskSizeGB(device string) (size string, err error) {
+	f, _ := ioutil.ReadFile(fmt.Sprintf("/sys/block/%s/size", device))
+	s := strings.Replace(string(f), "\n", "", -1)
+	x, _ := strconv.Atoi(s)
+	//fmt.Printf("Device size: %d\n", x)
+
+	size = strconv.Itoa(x * 512 / 1000 / 1000 / 1000)
+	//fmt.Printf("SIZE: %d\n", size)
+
+	return size, err
 }
